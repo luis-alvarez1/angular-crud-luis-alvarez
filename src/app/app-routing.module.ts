f96f283da18c874548e25a0d2dd848229bc6d52e
Luis Alvarez <lealvarezp@unac.edu.co>
2020-10-07 19:47:49 -0500

    entrega del ejercicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { CrudAddComponent } from './crud-add/crud-add.component';
+import { CrudEditComponent } from './crud-edit/crud-edit.component';
+import { CrudGetComponent } from './crud-get/crud-get.component';
+const routes: Routes = [
+  {
+    path: 'business/create',
+    component: CrudAddComponent,
+  },
+  {
+    path: 'business/edit/:id',
+    component: CrudEditComponent,
+  },
+  {
+    path: 'business',
+    component: CrudGetComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CrudAddComponent } from './crud-add/crud-add.component';
+import { CrudGetComponent } from './crud-get/crud-get.component';
+import { CrudEditComponent } from './crud-edit/crud-edit.component';
+import { BusinessService } from './business.service';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    CrudAddComponent,
+    CrudGetComponent,
+    CrudEditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BusinessService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
